70562ca24b9f9251635d4f657b15fdbf2d206dc7
Dejan <dejan.mijakovac@gmail.com>
2020-04-28 23:46:31 +0100

    Bla

--- src/GUI/ViewTripGUI.java
+++ src/GUI/ViewTripGUI.java
@@ -208,7 +208,7 @@ public class ViewTripGUI extends javax.swing.JFrame {
             headerPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, headerPnlLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 72, Short.MAX_VALUE)
+                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 74, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
